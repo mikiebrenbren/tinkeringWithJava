1f5d193580a486320a08f5cf5a345c55d2f9df80
mikieb <brennankennm@yahoo.com>
2014-12-30 00:07:05 -0700

    First Commits to for the basic treehouse code
     Please enter the commit message for your changes. Lines starting

---
+++ Intro.java
@@ -0,0 +1,11 @@
+
+public class Intro {
+
+	public static void main(String[] args){
+		String s = System.console().readLine("What is your name? ");
+		System.console().printf("Hello my name is %s!!", s);
+
+}
+
+
+}
